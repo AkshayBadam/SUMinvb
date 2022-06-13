18145a1bfe959abf9d51c4fd2306336f8c4f32da
Akshay Badam <Akshay_Badam@epam.com>
2022-06-13 23:04:44 +0530

    Add project files.

---
+++ SUMinvb/Program.vb
@@ -0,0 +1,16 @@
+Imports System
+
+
+
+Module Module1
+    Sub Main()
+        Dim a As Integer
+        Dim b As Integer
+        Dim sum As Integer
+        a = 10
+        b = 5
+        sum = a + b
+        Console.WriteLine("Sum of two numbers: " & (sum))
+        Console.ReadLine()
+    End Sub
+End Module
